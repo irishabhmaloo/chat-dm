471894f217ccd44dfefb6aa708bdf323d0653424
RishabhMaloo <RishabhMaloo@IA-5CD012D3KW>
2023-06-26 17:04:59 +0530

    initial JOIN design

--- src/Component/Join/Join.js
+++ src/Component/Join/Join.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import '../../Style/Join.css';
+import logo from '../../Images/logo.png';
 
 const Join = () => {
   return (
-    <div>
-      <h1>JOIN PAGE</h1>
+    <div className='JoinPage'>
+      <div className='JoinContainer'>
+        <img src={logo} alt='logo'/>
+        <h1>Chat-DM</h1>
+      </div>
     </div>
   )
 }
