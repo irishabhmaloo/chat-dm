42a9504cc375944c44adf2ffdd99dfd25f44b19a
RishabhMaloo <RishabhMaloo@IA-5CD012D3KW>
2023-06-27 00:09:26 +0530

    setting user input store

--- src/Component/Join/Join.js
+++ src/Component/Join/Join.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import '../../Style/Join.css';
 import logo from '../../Images/logo.png';
+import {Link} from 'react-router-dom';
+
+let user;
 
 const Join = () => {
+
+  const sendUser = () => {
+    user = document.getElementById('JoinInput').value;
+  }
+
   return (
     <div className='JoinPage'>
       <div className='JoinContainer'>
@@ -10,10 +18,13 @@ const Join = () => {
         <h1>Chat-DM</h1>
 
         <input type="text" id="JoinInput" placeholder='Enter your name' />
-        <button className='JoinBtn'>Login</button>
+        <Link to='/chat'>
+          <button onClick={sendUser} className='JoinBtn'>Login</button>
+        </Link>
       </div>
     </div>
   )
 }
 
 export default Join
+export {user}
