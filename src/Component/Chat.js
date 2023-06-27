c386ed3127370e9c391d062a31e5a79b4dd0dc7b
RishabhMaloo <RishabhMaloo@IA-5CD012D3KW>
2023-06-27 12:13:13 +0530

    chat area basic design final

--- src/Component/Chat.js
+++ src/Component/Chat.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import {user} from './Join';
 import socketIO from 'socket.io-client';
 import "../Style/Chat.css";
+import sendLogo from '../Images/send.png';
 
 // endpoint = server URL
 const ENDPOINT = "http://localhost:4500";
@@ -27,7 +28,10 @@ const Chat = () => {
       <div className='chatContainer'>
         <div className='header'></div>
         <div className='chatBox'></div>
-        <div className='inputBox'></div>
+        <div className='inputBox'>
+          <input type='text' id='chatInput' />
+          <button className='sendBtn'> <img src={sendLogo} alt="send logo" /> </button>
+        </div>
       </div>
       
     </div>
