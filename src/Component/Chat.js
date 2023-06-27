c8e2c8ca1d93968ad7bfdc1470311d883b6c84f2
RishabhMaloo <RishabhMaloo@IA-5CD012D3KW>
2023-06-27 11:09:54 +0530

    established connection with server

--- src/Component/Chat.js
+++ src/Component/Chat.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {user} from './Join';
+import socketIO from 'socket.io-client';
+
+// endpoint = server URL
+const ENDPOINT = "http://localhost:4500";
 
 const Chat = () => {
+
+  // creating socket
+  const socket = socketIO(ENDPOINT, {transports: ['websocket']});
+
+  // on connect
+  useEffect(() => {
+    socket.on("connect", ()=>{
+      alert("Connection established!");
+    });
+
+    return () => {
+
+    }
+  }, [socket]);
+  
   return (
-    <div>
-      <h1>{user}</h1>
+    <div className='chatPage'>
+      <div className='chatContainer'>
+        <div className='header'></div>
+        <div className='chatBox'></div>
+        <div className='inputBox'></div>
+      </div>
+      
     </div>
   )
 }
